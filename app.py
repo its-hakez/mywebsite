1bb20580941b34613e7cfca762f69d5be943bea8
Aditya Maharjan <adityamaharjan86@gmail.com>
2024-12-27 15:53:45 +0545

    Initial commit of portfolio3

--- app.py
+++ app.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home route
 @app.route('/')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+# About route
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# Contact route
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# Run the application
+if __name__ == "__main__":
+    app.run(debug=True)
